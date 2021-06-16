4abbaa990e740e45aeb47dd201a267d5acccdd32
Samantha Malone <sammac99@gmail.com>
2021-06-15 22:37:33 -0700

    added initial local storage., but still needs tweaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,14 @@ var apiKey = "bl5whSB0fw8HbwziUfOz6cNj3TYl8Wkx";
 searchForm.submit (searchLyrics);
 
 // add local storage
+searchLyrics();
+getenterArtist();
+getenterTitle();
 
+// lyrics ovh api
 function searchLyrics(event) {
     event.preventDefault();
-    var searchInputTitle = $(".searchInputTitle").val();
+    var searchInputTitle = $(".searchInputTitle").val() = localStorage.getItem();
     var searchInputArtist = $(".searchInputArtist").val();
     var lyricsDiv = document.querySelector (".lyrics-div");
     // error with loading site 504
@@ -28,6 +32,21 @@ function searchLyrics(event) {
   </div>
 </div>
         `)
+        // local storage
+        function getenterArtist() {
+          var teList = JSON.parse(localStorage.getItem(enterArtistName));
+
+          if (teList) {
+            enterArtist = teList
+          }
+        }
+        function getenterTitle() {
+          var teList = JSON.parse(localStorage.getItem(enterTitleName));
+
+          if (teList) {
+            enterTitle = teList
+          }
+        }
     });
   };
 // Create a function called `searchLyrics()`
@@ -47,71 +66,7 @@ function gifMe(event) {
     .catch(function (error) {
         console.log(error);
     }) 
-  }; 
- 
-  
-  // function waitingGif(event) {
-  //   event.preventDefault();
-  //   var str = document.querySelector ("lyrics.div");
-  //   fetch
-  // }
 
-  // document.addEventListener("DOMContentLoaded", init);
-  // function init () {
-  //   // error message here HELP!
-  //   // document.getElementById("btnSearch").addEventListener("click", ev => {
-  //     $("#searchBtn").on("submit", ev => {
-  //       // to stop page reload
-  //     ev.preventDefault();
-  //     var str = document.getElementById("search").nodeValue.trim();
-  //     url = url.concat(str);
-  //     console.log(url);
-  //     fetch(url)
-  //       .then(response => response.json())
-  //       .then(content => {
-  //         // data, pagination, meta
-  //         console.log(content.data);
-  //         console.log("META", content.meta);
-  //         var fig = document.createElement("figure");
-  //         var img = document.createElement("img");
-  //         var fc = document.createElement("figcaption");
-  //         img.src = content.data[0].images.downsized.url;
-  //         img.alt = content.data[0].title;
-  //         fc.textContent = content.data[0].title;
-  //         fig.appendChild(img);
-  //         fig.appendChild(fc);
-  //         var out = document.querySelector("out");
-  //       });
-  //     })
-  //   };
-// example from class work
-// function gifMe() {
-//   // Create a variable called `searchTerm` that will use `document.querySelector()` to target the `id` of the input
-//   // Use `.value` to capture the value of the input and store it in the variable
-//   // var searchForm = document.querySelector('#searchTerm').value;
-
-//   // Make a `fetch` request concatenating the `searchTerm` to the query URL
-//   // Remember to add your API key at the end
-//   fetch(
-//     'https://api.giphy.com/v1/gifs/search?q=' +
-//       searchForm +
-//       '&api_key=bl5whSB0fw8HbwziUfOz6cNj3TYl8Wkx'
-//   )
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(response) {
-//       console.log(response.data[0]);
-//       // Create a variable that will select the <div> where the GIF will be displayed
-//       var responseContainerEl = document.querySelector('#response-container');
-
-//       // Empty out the <div> before we append a GIF to it
-//       responseContainerEl.innerHTML = '';
-
-//       var gifImg = document.createElement('img');
-//       gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
-
-//       // Append 'gifImg' to the <div>
-//       responseContainerEl.appendChild(gifImg);
-//     });
-// };   
+    // store in local Storage
+    localStorage.setItem(enterArtistName, enterTitleName, JSON.stringify(enterArtist, enterTitle));
+  };   
