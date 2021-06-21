0f95d3b735cc515afba4133465f2661632e73aec
Marco Avila <mavila6@email.arizona.com>
2021-06-20 23:05:17 -0700

    added comments to remaining script files and some to html

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,14 @@
+//Used arrow notation to perform function called navslide to reveal navbar upon a click
 const navSlide = () => {
-    const burger = document.querySelector('#burger')
-    const nav = document.querySelector('#sidebar')
-    const darkMode = document.querySelector('#meatball-container')
-    const mobileNavBtn1 = document.querySelector('#mobileNavBtn1')
-    const mobileNavBtn2 = document.querySelector('#mobileNavBtn2')
-    const mobileNavBtn3 = document.querySelector('#mobileNavBtn3')
+    //variables linking up to Ids on the html 
+    const burger = document.querySelector('#burger');
+    const nav = document.querySelector('#sidebar');
+    const darkMode = document.querySelector('#meatball-container');
+    const mobileNavBtn1 = document.querySelector('#mobileNavBtn1');
+    const mobileNavBtn2 = document.querySelector('#mobileNavBtn2');
+    const mobileNavBtn3 = document.querySelector('#mobileNavBtn3');
 
-
+//added event listener to element with burger id to open navbar on a click
     burger.addEventListener('click', () => {
         nav.classList.toggle('sidebar-active')
         darkMode.classList.add('hide')
@@ -15,5 +17,5 @@ const navSlide = () => {
         mobileNavBtn3.classList.toggle('hide')
     })
 }
-
+//called the navslide function
 navSlide();
