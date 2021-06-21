2e49f603e0e92a42e6872e665902e4a0e4664fb4
Marco Avila <mavila6@email.arizona.com>
2021-06-21 00:22:03 -0700

    refactored html with repetetive code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
+//Variable used as local storage values for search persistence
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 //variables representing elements on the page to append datato html
 var searchForm = $('.searchForm');
 var title = $('#title');
 var artist = $('.artist');
 var img = $('#gif-embed');
 var divEl = $('.content');
-//variables containing the input values for the search based on what the user submits
-var searchInputTitle = $('.searchInputTitle').val();
-var searchInputArtist = $('.searchInputArtist').val();
 //calling gifme function to load giphy upon openeing the application
 gifMe();
 //added event listener to run a function that saves the search inputs for the user on submit
 searchForm.on('submit', function(event) {
     event.preventDefault();
-    //Variables used as local storage values for search persistence
-    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-    var searchValue = searchInputTitle + " by: " + searchInputArtist;
+    //variables containing the input values for the search based on what the user submits
+    var searchInputTitle = $('.searchInputTitle').val();
+    var searchInputArtist = $('.searchInputArtist').val();
+    var searchValue = searchInputTitle + " by: " + searchInputArtist
     searchHistory.unshift(searchValue);
     localStorage.setItem("search", JSON.stringify(searchHistory));
     //calling the searchlyrics function with the users search parameters
